ea1cc4ed9d186ca7dbbf22314b41e798e6cc607a
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-17 00:30:35 +0100

    Add basic ember template

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  mount_ember_app :frontend, to: "/"
   root to: 'visitors#index'
-  get '/auth/:provider/callback' => 'sessions#create'
-  get '/signin' => 'sessions#new', :as => :signin
-  get '/signout' => 'sessions#destroy', :as => :signout
-  get '/auth/failure' => 'sessions#failure'
+
+  # get '/auth/:provider/callback' => 'sessions#create'
+  # get '/signin' => 'sessions#new', :as => :signin
+  # get '/signout' => 'sessions#destroy', :as => :signout
+  # get '/auth/failure' => 'sessions#failure'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
