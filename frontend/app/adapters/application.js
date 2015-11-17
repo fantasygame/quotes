5e4f0250b9d31afc47a3a63d648492fec30d127f
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-17 20:58:08 +0100

    Add active model adapter

---
+++ frontend/app/adapters/application.js
@@ -0,0 +1,9 @@
+import ActiveModelAdapter from 'active-model-adapter';
+import $ from 'jquery';
+
+export default ActiveModelAdapter.extend({
+  namespace: 'api',
+  headers: {
+    "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
+  }
+});
