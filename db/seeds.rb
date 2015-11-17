44f2de41a8b86761a1ed6e0bfd0754fbebcbf69e
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-17 20:43:17 +0100

    Add basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Quote.create(content: 'Funny quote. Very funny.')
+Quote.create(content: 'Another funny quote. Not so funny.')
