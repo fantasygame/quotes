d48f5d1f4240e837a9b8bb46533c8285a93b2583
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-19 22:43:22 +0100

    Setup google oath with application.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/signin' => 'sessions#new', as: :signin
+  get '/signout' => 'sessions#destroy', as: :signout
+  get '/auth/failure' => 'sessions#failure'
+
   namespace :api do
     resources :quotes
   end
   mount_ember_app :frontend, to: "/"
   mount EmberCLI::Engine => 'ember-tests' if Rails.env.development?
   root to: 'visitors#index'
-
-  # get '/auth/:provider/callback' => 'sessions#create'
-  # get '/signin' => 'sessions#new', as: :signin
-  # get '/signout' => 'sessions#destroy', as: :signout
-  # get '/auth/failure' => 'sessions#failure'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
