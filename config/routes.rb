a1e1377b69367b82d18b65b5b9f6b5b83e221160
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-19 22:54:17 +0100

    Users controller api

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :quotes
+      resources :users, only: :show
     end
   end
   mount_ember_app :frontend, to: "/"
