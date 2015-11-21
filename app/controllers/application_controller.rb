ba39f232acc7ac4510a2add9660481086cb7b616
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-21 23:25:48 +0100

    Remove authenticate_user! action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user_from_token!
-  before_action :authenticate_user!
 
   private
 
@@ -11,7 +10,6 @@ class ApplicationController < ActionController::Base
     authenticate_with_http_token do |token, options|
       user_email = options[:email].presence
       user = user_email && User.find_by_email(user_email)
-
       if user && Devise.secure_compare(user.authentication_token, token)
         sign_in user, store: false
       end
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,5 @@
 class VisitorsController < ApplicationController
   skip_before_action :authenticate_user_from_token!
-  skip_before_action :authenticate_user!
 
   def index
     render layout: false
