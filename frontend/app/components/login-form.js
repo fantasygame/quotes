188904663894df4d43baf4c9879c587e2eb93c53
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-21 22:26:06 +0100

    Implement session authentication

--- frontend/app/components/login-form.js
+++ frontend/app/components/login-form.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
+const { service } = Ember.inject;
+
 export default Ember.Component.extend({
+  session: service('session'),
   actions: {
     authenticate: function() {
-      alert('hello');
+      let { identification, password } = this.getProperties('identification', 'password');
+      return this.get('session').authenticate('authenticator:devise', identification, password).catch((reason) => {
+        this.set('errorMessage', reason.error);
+      });
     }
   }
 });
