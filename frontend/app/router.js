0436ca69b7cf78187a20794a63902351c663575d
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-22 00:35:10 +0100

    Make quotes root url

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('quotes', function() {});
+  this.route('quotes', { path: '' }, function() {});
   this.route('users');
   this.route('login');
 });
