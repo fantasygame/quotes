86c144c8f1b3b1bb89af1afc1dcb4737115c30d4
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-21 21:30:58 +0100

    Use token to authenticate by application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,35 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  helper_method :current_user
-  helper_method :user_signed_in?
-  helper_method :correct_user?
+  before_action :authenticate_user_from_token!
+  before_action :authenticate_user!
 
   private
-    def current_user
-      begin
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
-      rescue Exception => e
-        nil
+
+  def authenticate_user_from_token!
+    authenticate_with_http_token do |token, options|
+      user_email = options[:email].presence
+      user = user_email && User.find_by_email(user_email)
+
+      if user && Devise.secure_compare(user.authentication_token, token)
+        sign_in user, store: false
       end
     end
-
-    def user_signed_in?
-      return true if current_user
-    end
-
-    def correct_user?
-      @user = User.find(params[:id])
-      unless current_user == @user
-        redirect_to root_url, :alert => "Access denied."
-      end
-    end
-
-    def authenticate_user!
-      if !current_user
-        redirect_to root_url, :alert => 'You need to sign in for access to this page.'
-      end
-    end
-
+  end
 end
