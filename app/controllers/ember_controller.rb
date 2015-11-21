b2d9ed7bde701e4aa0b6b6aab73a09afb76748ef
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-22 00:44:32 +0100

    Rename visitors controller to ember

--- app/controllers/ember_controller.rb
+++ app/controllers/ember_controller.rb
@@ -1,4 +1,4 @@
-class VisitorsController < ApplicationController
+class EmberController < ApplicationController
   skip_before_action :authenticate_user_from_token!
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
   mount_ember_app :frontend, to: "/"
   mount EmberCLI::Engine => 'ember-tests' if Rails.env.development?
-  root to: 'visitors#index'
+  root to: 'ember#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
