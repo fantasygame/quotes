2f221630aba6bfdfd723ff4c2afe5fc6a0543dd3
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-22 17:27:35 +0100

    Add uproot custom action in quote model

--- frontend/app/models/quote.js
+++ frontend/app/models/quote.js
@@ -1,7 +1,11 @@
 import DS from 'ember-data';
+import { memberAction, collectionAction } from 'ember-api-actions';
+
+const { attr } = DS;
 
 export default DS.Model.extend({
   content: DS.attr('string'),
   userId: DS.attr('number'),
-  voteBalance: DS.attr('number')
+  voteBalance: DS.attr('number'),
+  upvote: memberAction({ path: 'upvote' }),
 });
