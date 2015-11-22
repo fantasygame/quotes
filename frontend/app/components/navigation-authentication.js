e6a4a9e8f7a27e45b10a6d7bed91ec8f004fb09f
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-22 03:36:55 +0100

    Add current user to navigation

--- frontend/app/components/navigation-authentication.js
+++ frontend/app/components/navigation-authentication.js
@@ -3,6 +3,8 @@ const { service } = Ember.inject;
 
 export default Ember.Component.extend({
   session: service('session'),
+  currentUser: service('current-user'),
+
   actions: {
     logout() {
       this.get('session').invalidate();
