e900dce484be5577a7e5df3293adb6b56a485a4c
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-22 14:39:39 +0100

    Setup saving with current user

--- frontend/app/components/quotes/quote-form.js
+++ frontend/app/components/quotes/quote-form.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
+const { service } = Ember.inject;
+
 export default Ember.Component.extend({
+  session: service('session'),
   actions: {
     save() {
-      console.log('save');
+      this.set('quote.user_id', this.get('session.data.authenticated.user_id'));
+      this.get('quote').save().then((quote) => {
+        this.get('router').transitionTo('quotes');
+      });
     },
     cancel() {
       this.get('router').transitionTo('quotes');
