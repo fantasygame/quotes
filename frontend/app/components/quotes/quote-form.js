a0de7055e66164214c2b8cef0b4502d7d372c377
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-22 13:46:08 +0100

    Transition to quotes on form cancel

--- frontend/app/components/quotes/quote-form.js
+++ frontend/app/components/quotes/quote-form.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      console.log('save');
+    },
+    cancel() {
+      this.get('router').transitionTo('quotes');
+    }
+  }
 });
